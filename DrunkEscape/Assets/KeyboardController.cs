db8572f28d3d6504f6160e9ea4943ee3a0b0748c
Anton Reinhard <anton.reinhard@wandelbotsgmbh.onmicrosoft.com>
2019-11-09 12:03:50 +0100

    KeyboardController

---
+++ DrunkEscape/Assets/KeyboardController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       var forward_key = Input.GetKey(KeyCode.W);
+
+       if (forward_key)
+       {
+          transform.Translate(0.1f, 0, 0);
+       }
+    }
+
+    public GameObject source;
+
+    public BoxCollider box_collider;
+
+    public int speed;
+
+    public Transform target;
+}
