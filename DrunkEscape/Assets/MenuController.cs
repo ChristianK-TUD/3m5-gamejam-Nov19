a94f73e44cfd8898a8ca7db3cc9c4bc86a4a0176
ChristianK-TUD <ck-mail@gmx-topmail.de>
2019-11-10 14:29:07 +0100

    Menu

---
+++ DrunkEscape/Assets/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string levelName;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
